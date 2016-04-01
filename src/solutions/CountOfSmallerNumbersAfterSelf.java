f30d9affce930f69481305cdaed9a11fc7b3f1c0
CindyLiu617 <yunjia.g@gmail.com>
2016-03-31 19:15:18 -0700

    using BST to resolve count of smaller numbers after self problem

--- src/solutions/CountOfSmallerNumbersAfterSelf.java
+++ src/solutions/CountOfSmallerNumbersAfterSelf.java
@@ -1,7 +1,116 @@
 package solutions;
 
+import com.sun.tools.javac.util.Assert;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 /**
  * Created by James on 3/28/16.
  */
 public class CountOfSmallerNumbersAfterSelf {
+    public List<Integer> countSmaller(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return new ArrayList<Integer>();
+        Integer[] rst = new Integer[nums.length];
+        Node root = new Node(nums[nums.length - 1]);
+        rst[nums.length - 1] = 0;
+        for (int i = nums.length - 2; i >= 0; --i){
+            int count = BSTInsertNode(root, nums[i]);
+            rst[i] = count;
+        }
+        return Arrays.asList(rst);
+    }
+
+    private int BSTInsertNode(Node root, int val){
+        int smallerCount = 0;
+        while (root != null){
+            if (val <= root.getValue()){
+                root.plusOneCount();
+                if( root.getLeft() == null){
+                    root.setLeft(val);
+                    break;
+                }
+                else
+                    root = root.getLeft();
+            }
+            else{
+                smallerCount += root.getCount();
+                if (root.getRight() == null){
+                    root.setRight(val);
+                    break;
+                }
+                else
+                    root = root.getRight();
+            }
+        }
+        return smallerCount;
+    }
+
+    class Node{
+        private int value;
+        private Node left;
+        private Node right;
+        private int count = 1;
+
+        public Node(int val){
+            value = val;
+        }
+
+        public int getValue(){
+            return this.value;
+        }
+
+        public Node getLeft(){
+            return this.left;
+        }
+
+        public void setLeft(int val){
+            Node newNode = new Node(val);
+            this.left = newNode;
+        }
+
+        public Node getRight(){
+            return this.right;
+        }
+
+        public void setRight(int val){
+            Node newNode = new Node(val);
+            this.right = newNode;
+        }
+
+        public int getCount(){
+            return count;
+        }
+
+        public void plusOneCount(){
+            this.count += 1;
+        }
+    }
+
+    static public class Test {
+        static private CountOfSmallerNumbersAfterSelf _solution = new CountOfSmallerNumbersAfterSelf();
+
+        static public void test(int[] A, List<Integer> ans) {
+            for (int i = 0; i < A.length; ++i) {
+                Assert.check(ans.get(i).equals(_solution.countSmaller(A).get(i)));
+            }
+        }
+
+        static public void randomTest() {
+//            int[] nums = {5,2,6,1};
+//            test(nums, new ArrayList<Integer>(Arrays.asList(2,1,1,0)));
+
+//            int[] nums2 = {1};
+//            test(nums2, new ArrayList<Integer>(Collections.singletonList(0)));
+//
+//            int[] nums3 = {1, 2, 3, 4};
+//            test(nums3, new ArrayList<Integer>(Arrays.asList(0, 0, 0, 0)));
+
+            int[] nums4 = {4, 3, 2, 1};
+            test(nums4, new ArrayList<Integer>(asList(3, 2, 1, 0)));
+        }
+    }
 }
