746fe85a768794881d4a225692078ea7dd143d59
CindyLiu617 <yunjia.g@gmail.com>
2016-04-06 22:58:25 -0700

    using merge sort to find minimum meeting rooms under multiply time ranges

---
+++ src/solutions/MeetingRoomsII.java
@@ -0,0 +1,7 @@
+package solutions;
+
+/**
+ * Created by James on 4/6/16.
+ */
+public class MeetingRoomsII {
+}
---
+++ src/solutions/utils/Interval.java
@@ -0,0 +1,7 @@
+package solutions.utils;
+
+/**
+ * Created by James on 4/3/16.
+ */
+public class Interval {
+}
