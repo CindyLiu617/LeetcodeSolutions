ea5f28cc2b0eca9a2ec816c2d88a71a1ed09e2b4
CindyLiu617 <yunjia.g@gmail.com>
2016-04-13 22:38:17 -0700

    updated code for container with most water - leetcode

--- src/solutions/ContainerWithMostWater.java
+++ src/solutions/ContainerWithMostWater.java
@@ -1,7 +1,35 @@
 package solutions;
 
+import com.sun.tools.javac.util.Assert;
+
 /**
  * Created by James on 4/13/16.
  */
 public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0)
+            return 0;
+        int len = height.length, left = 0, right = len - 1, maxWater = 0;
+        while (left >= 0 && right <= len - 1 && left <= right){
+            maxWater = Math.max(maxWater, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]){
+                ++left;
+            }
+            else
+                --right;
+        }
+        return maxWater;
+    }
+    static public class Test {
+        static private ContainerWithMostWater _solution = new ContainerWithMostWater();
+
+        static public void test(int[] height, int ans) {
+            Assert.check(ans == (_solution.maxArea(height)));
+        }
+
+        static public void randomTest() {
+            int[] height = {5, 15, 20, 2};
+            test(height, 15);
+        }
+    }
 }
