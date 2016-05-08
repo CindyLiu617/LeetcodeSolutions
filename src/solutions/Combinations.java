970a6ecd96de976bb3eb00410ab258aa54845d18
CindyLiu617 <yunjia.g@gmail.com>
2016-05-07 21:56:48 -0700

    leetcode - Combinations & LargestRectangleInHistogram

---
+++ src/solutions/Combinations.java
@@ -0,0 +1,46 @@
+package solutions;
+
+import com.sun.tools.javac.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by James on 5/4/16.
+ */
+public class Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        if (n < 0)
+            return null;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> path = new ArrayList<Integer>();
+        return combineHelper(result, path, k, n, 0, 1);
+    }
+
+    private List<List<Integer>> combineHelper(List<List<Integer>> result, List<Integer> path,
+                                              int k, int n, int count, int start){
+        for (int i = start; i <= n+1; ++i){
+            if (count == k){
+                result.add(new ArrayList<Integer>(path));
+                return result;
+            }
+            path.add(i);
+            ++count;
+            combineHelper(result, path, k, n, count, i+1);
+            path.remove(path.size() - 1);
+            --count;
+        }
+        return result;
+    }
+    static public class Test {
+        static private Combinations _solution = new Combinations();
+
+        static public void test(int n, int k, List<List<Integer>> ans) {
+            Assert.check(ans == (_solution.combine(n, k)));
+        }
+
+        static public void randomTest() {
+
+        }
+    }
+}
---
+++ src/solutions/LargestRectangleInHistogram.java
@@ -0,0 +1,59 @@
+package solutions;
+
+import com.sun.tools.javac.util.Assert;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * Created by James on 4/26/16.
+ */
+public class LargestRectangleInHistogram {
+    public int largestRectangleArea(int[] heights){
+        if (heights == null || heights.length == 0)
+                return 0;
+        Stack<Integer> myStack = new Stack<Integer>();
+        int curHighestIndex, maxArea = 0, i = 0;
+        int[] helperArray = Arrays.copyOf(heights, heights.length + 1);
+        while (i < helperArray.length){
+            if (myStack.isEmpty() || helperArray[i] >= helperArray[myStack.peek()]){
+                myStack.push(i);
+                ++i;
+            }
+            else {
+                curHighestIndex = myStack.peek();
+                myStack.pop();
+                if (!myStack.isEmpty())
+                    maxArea = Math.max(helperArray[curHighestIndex] * (i - myStack.peek() - 1), maxArea);
+//                BE CAREFUL!!
+                else
+                    maxArea = Math.max(helperArray[curHighestIndex] * i, maxArea);
+            }
+        }
+        return maxArea;
+    }
+    static public class Test {
+        static private LargestRectangleInHistogram _solution = new LargestRectangleInHistogram();
+
+        static public void test(int[] s, int ans) {
+            Assert.check(ans == (_solution.largestRectangleArea(s)));
+        }
+
+        static public void randomTest() {
+//            int[] array1 = {2,1,5,6,2,3};
+//            test(array1, 10);
+//
+//            int[] array2 = {1};
+//            test(array2, 1);
+//
+//            int[] array3 = {1,1};
+//            test(array3, 2);
+//
+//            int[] array4 = {2,1,2};
+//            test(array4, 3);
+//
+            int[] array5 = {4,2,0,3,2,5};
+            test(array5, 6);
+        }
+    }
+}
