a9fd46515a7d6c277e81e84d6e662ca36c061d38
CindyLiu617 <yunjia.g@gmail.com>
2016-07-09 23:17:58 -0700

    Solution for RemoveDuplicateLetters -- leetcode (not completed)

--- src/solutions/RemoveDuplicateLetters.java
+++ src/solutions/RemoveDuplicateLetters.java
@@ -5,18 +5,17 @@ package solutions;
  */
 public class RemoveDuplicateLetters {
     public String removeDuplicateLetters(String s) {
+        if (s == null || s.length() == 0)
+            return null;
+        //letters are 26 characters
         int[] cnt = new int[26];
-        int pos = 0; // the position for the smallest s[i]
+        // the position for the smallest s[i]
+        int pos = 0;
         for (int i = 0; i < s.length(); i++)
             cnt[s.charAt(i) - 'a']++;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) < s.charAt(pos))
                 pos = i;
-            System.out.println();
-            System.out.println(s.charAt(i));
-            System.out.println(s.charAt(i) - 'a');
-            System.out.println(cnt[s.charAt(i) - 'a']);
-            System.out.println();
             if (--cnt[s.charAt(i) - 'a'] == 0)
                 break;
         }
