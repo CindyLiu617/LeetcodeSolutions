3b3a531c0dd62561e37216c14f9d8f403dba4a88
CindyLiu617 <yunjia.g@gmail.com>
2016-10-19 14:38:27 -0700

    Solution for ContainsDuplicate -- leetcode using set

---
+++ src/solutions/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+package solutions;
+
+import java.util.HashSet;
+
+/**
+ * Created by James on 10/19/16.
+ */
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; ++i){
+            if (!set.contains(nums[i])){
+                set.add(nums[i]);
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
