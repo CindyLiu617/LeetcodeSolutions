960cca504f919ef94f0222a326fd1a99c8c87be0
CindyLiu617 <yunjia.g@gmail.com>
2016-11-02 20:36:58 -0700

    RandomListNode

---
+++ src/solutions/utils/RandomListNode.java
@@ -0,0 +1,12 @@
+package solutions.utils;
+
+/**
+ * Created by James on 10/31/16.
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next, random;
+    RandomListNode(int x){
+        this.label = x;
+    }
+}
