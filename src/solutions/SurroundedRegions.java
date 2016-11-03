8b43532ab843934fe2b1d0b425824ccf08ea70b0
CindyLiu617 <yunjia.g@gmail.com>
2016-11-02 22:00:23 -0700

    Solution for SurroundedRegions -- Lintcode BFS

--- src/solutions/SurroundedRegions.java
+++ src/solutions/SurroundedRegions.java
@@ -2,10 +2,12 @@ package solutions;
 
 import java.util.LinkedList;
 
-/**
- * Created by James on 6/8/16.
- */
 public class SurroundedRegions {
+    /**
+     * @param board a 2D board containing 'X' and 'O'
+     * @return void
+     */
+    //BFS
     public void surroundedRegions(char[][] board) {
         if(board==null || board.length<=1 || board[0].length<=1)
             return;
@@ -13,10 +15,12 @@ public class SurroundedRegions {
             fill(board,0,i);
             fill(board,board.length-1,i);
         }
+
         for(int i=0;i<board.length;i++){
             fill(board,i,0);
             fill(board,i,board[0].length-1);
         }
+
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]=='O')
@@ -27,31 +31,31 @@ public class SurroundedRegions {
         }
     }
     private void fill(char[][] board, int i, int j){
-        if(board[i][j]!='O')
-            return;
-        board[i][j] = '#';
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-        int code = i*board[0].length + j;
-        queue.add(code);
-        while(!queue.isEmpty()){
-            code = queue.poll();
-            int row = code/board[0].length;
-            int col = code%board[0].length;
-            if(row >= 1 && board[row-1][col] == 'O'){
-                queue.add((row-1)*board[0].length + col);
-                board[row-1][col]='#';
-            }
-            if(row <= board.length-2 && board[row+1][col] == 'O'){
-                queue.add((row+1)*board[0].length + col);
-                board[row+1][col]='#';
-            }
-            if(col >= 1 && board[row][col-1] == 'O') {
-                queue.add(row * board[0].length + col - 1);
-                board[row][col - 1] = '#';
-            }
-            if(col <= board[0].length-2 && board[row][col+1] == 'O'){
-                queue.add(row*board[0].length + col+1);
-                board[row][col+1]='#';
+        if(board[i][j]=='O'){
+            board[i][j] = '#';
+            LinkedList<Integer> queue = new LinkedList<Integer>();
+            int code = i*board[0].length + j;
+            queue.add(code);
+            while(!queue.isEmpty()){
+                code = queue.poll();
+                int row = code/board[0].length;
+                int col = code%board[0].length;
+                if(row >= 1 && board[row-1][col] == 'O'){
+                    queue.add((row-1)*board[0].length + col);
+                    board[row-1][col]='#';
+                }
+                if(row <= board.length-2 && board[row+1][col] == 'O'){
+                    queue.add((row+1)*board[0].length + col);
+                    board[row+1][col]='#';
+                }
+                if(col >= 1 && board[row][col-1] == 'O') {
+                    queue.add(row * board[0].length + col - 1);
+                    board[row][col - 1] = '#';
+                }
+                if(col <= board[0].length-2 && board[row][col+1] == 'O'){
+                    queue.add(row*board[0].length + col+1);
+                    board[row][col+1]='#';
+                }
             }
         }
     }
