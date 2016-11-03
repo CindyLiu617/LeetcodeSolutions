89ae34d0845891a8ba0f6c868417fd9139f83aec
CindyLiu617 <yunjia.g@gmail.com>
2016-11-02 20:38:11 -0700

    Solution for GraphValidTree -- lintcode using Union Find

--- src/solutions/GraphValidTree.java
+++ src/solutions/GraphValidTree.java
@@ -7,35 +7,42 @@ package solutions;
  */
 public class GraphValidTree {
     public boolean validTree(int n, int[][] edges) {
-        int[] ids = new int[n];
+        //union find
+        int[] root = new int[n];
+        //initiate every node's parent to 0, 1, ,,,n respectively.
+        //precondition: Given n nodes labeled from 0 to n - 1
         for (int i = 1; i < n; ++i)
-            ids[i] = i;
+            root[i] = i;
         for (int[] edge : edges) {
-            int root1 = _find(edge[0], ids);
-            int root2 = _find(edge[1], ids);
+            int root1 = _find(edge[0], root);
+            int root2 = _find(edge[1], root);
+            //the original roots are same before union, meaning they are
+            // already connected, then if a new edge is added, a cycle is
+            // created.
             if (root1 == root2)
                 return false;
-            ids[root2] = root1;
+            //union: set edge[0]'s parent points to edge[1]
+            //or root[root2] = root1
+            root[root1] = root2;
         }
         return edges.length == n - 1;
     }
 
+    //find original root
     private int _find(int node, int[] ids) {
         while (ids[node] != node)
             node = ids[node];
         return node;
     }
+
     static public class Test {
         static private GraphValidTree _solution = new GraphValidTree();
 
-        static public void test(int n, int[][] edges, boolean ans) {
-            assert ans == (_solution.validTree(n, edges));
-        }
-
         static public void randomTest() {
-            int[][] edges = {{0, 1}, {1, 2}, {2, 3}, {1, 3}, {1, 4}};
+            int[][] edges = {{15, 16}, {16, 17}};
             int n = 5;
-            test(n, edges, false);
+            boolean rst = _solution.validTree(n, edges);
+            System.out.println(rst);
         }
     }
 }
