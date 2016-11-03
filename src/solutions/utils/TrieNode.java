f7fc33eebd3214675839524a011e434aa259b5b3
CindyLiu617 <yunjia.g@gmail.com>
2016-11-02 20:35:50 -0700

    Revised TrieNode

--- src/solutions/utils/TrieNode.java
+++ src/solutions/utils/TrieNode.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
  * Created by James on 5/16/16.
  */
 public class TrieNode {
-    char c;
-    boolean leaf;
-    HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+    public char c;
+    public boolean leaf;
+    public HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
     public TrieNode(char c){
         this. c = c;
     }
