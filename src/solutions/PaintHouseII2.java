c121d7f32d7d9d2d1fcbf135a03bf87d1f43e186
CindyLiu617 <yunjia.g@gmail.com>
2016-11-05 13:28:33 -0700

    move classes to solutions

---
+++ src/solutions/PaintHouseII2.java
@@ -0,0 +1,7 @@
+package solutions;
+
+/**
+ * Created by James on 11/5/16.
+ */
+public class PaintHouseII2 {
+}
--- src/solutions/ReverseWordsInAString.java
+++ src/solutions/ReverseWordsInAString.java
@@ -1,4 +1,4 @@
-import solutions.ContainsDuplicateIII;
+package solutions;
 
 /**
  * Created by James on 10/31/16.
