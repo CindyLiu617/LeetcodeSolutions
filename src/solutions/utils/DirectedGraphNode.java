9eef3a3cb8dc33816bd222b4b1c89143489e452c
CindyLiu617 <yunjia.g@gmail.com>
2016-11-10 12:15:11 -0800

    Util.DirectedGraphNode -- Lintcode

--- src/solutions/utils/DirectedGraphNode.java
+++ src/solutions/utils/DirectedGraphNode.java
@@ -2,13 +2,10 @@ package solutions.utils;
 
 import java.util.ArrayList;
 
-/**
- * Created by James on 7/10/16.
- */
 public class DirectedGraphNode {
     public int label;
     public ArrayList<DirectedGraphNode> neighbors;
-    DirectedGraphNode(int x){
+    public DirectedGraphNode(int x){
         label = x;
         neighbors = new ArrayList<DirectedGraphNode>();
     }
