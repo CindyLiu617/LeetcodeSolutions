07fd38eff5374b5786117f45e258651d6a05df4c
CindyLiu617 <yunjia.g@gmail.com>
2016-11-11 18:03:10 -0800

    Solution for CombinationSum -- Lintcode Recursion

---
+++ src/solutions/CombinationSum.java
@@ -0,0 +1,59 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum {
+    /**
+     * @param candidates: A list of integers
+     * @param target:An integer
+     * @return: A list of lists of integers
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // write your code here
+        List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        if (candidates == null || candidates.length == 0) {
+            return rst;
+        }
+        List<Integer> combination = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        helper(rst, combination, candidates, target, 0);
+        return rst;
+    }
+
+    private void helper(List<List<Integer>> rst, List<Integer> combination,
+                        int[] candidates, int target, int ptr) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            rst.add(new ArrayList<Integer>(combination));
+            return;
+        }
+        for (int i = ptr; i < candidates.length; ++i) {
+            //当candidates[i] == candidates[i- 1] 时，说明此组合之前已经有过，如果不跳过此种情况
+            //则会重复
+            if (i > 0 && candidates[i] == candidates[i- 1]) {
+                continue;
+            }
+            combination.add(candidates[i]);
+            helper(rst, combination, candidates, target - candidates[i], i);
+            combination.remove(combination.size() - 1);
+        }
+    }
+    static public class Test {
+        static private CombinationSum _solution = new CombinationSum();
+
+        static public void randomTest() {
+            int[] nums = {2, 2};
+            List<List<Integer>> rst = _solution.combinationSum(nums, 4);
+            for (int i = 0; i < rst.size(); ++i) {
+                for (int j = 0; j < rst.get(i).size(); ++j) {
+                    System.out.print(rst.get(i).get(j));
+                }
+                System.out.println(">>>>>>>>");
+            }
+        }
+    }
+}
