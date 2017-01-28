4761c2e3567f6a526311f886a069df8316f975dc
CindyLiu617 <yunjia.g@gmail.com>
2017-01-28 17:53:07 +0800

    Solution for TargetSum -- Leetcode DP

--- src/solutions/TargetSum.java
+++ src/solutions/TargetSum.java
@@ -24,14 +24,48 @@ public class TargetSum {
         helper(nums, sum + nums[cur], target, cur + 1);
         helper(nums, sum - nums[cur], target, cur + 1);
     }
-    
+
+    //DP
+    //in other word: sum(pos) - |sum(neg)| = target
+    //sum(pos) + |sum(neg)| + sum(pos) - |sum(neg)| = target + sum(pos) +
+    // |sum(neg)|
+    //sum(pos) * 2 = target + sum(nums)
+    public int findTargetSumWays2(int[] nums, int S) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (Integer num : nums) {
+            sum += num;
+        }
+        if ((S + sum) % 2 != 0 || sum < S) {
+            return 0;
+        }
+        return genSubset(nums, (S + sum) / 2);
+    }
+
+    //find num of subsets of nums that sum(subset) = (target + sum(nums)) / 2
+    //Backpack V problem
+    private int genSubset (int[] nums, int bagsize) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[bagsize + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = bagsize; j >= nums[i]; --j) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[bagsize];
+    }
 
     static public class Test {
         static private TargetSum _solution = new TargetSum();
 
         static public void randomTest() {
             int[] nums = {1, 1, 1, 1, 1};
-            System.out.println(_solution.findTargetSumWays(nums, 3));
+            System.out.println(_solution.findTargetSumWays2(nums, 3));
 
         }
     }
