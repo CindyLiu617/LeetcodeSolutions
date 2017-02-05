e9857d5afa73247186d0bf63f6fd661a00621da4
CindyLiu617 <yunjia.g@gmail.com>
2017-02-05 16:05:32 +0800

    Solution for InsertionSortList -- Leetcode Insertion Sort

--- src/solutions/InsertionSortList.java
+++ src/solutions/InsertionSortList.java
@@ -4,29 +4,24 @@ import solutions.utils.ListNode;
 
 public class InsertionSortList {
     public ListNode insertionSortList(ListNode head) {
-        if( head == null ){
-            return head;
+        if (head == null) {
+            return null;
         }
-
-        ListNode newHead = new ListNode(0); //new starter of the sorted list
-        ListNode cur = head; //the node will be inserted
-        ListNode pre = newHead; //insert node between pre and pre.next
-        ListNode next = null; //the next node will be inserted
-        //not the end of input list
-        while( cur != null ){
-            next = cur.next;
-            //find the right place to insert
-            while( pre.next != null && pre.next.val < cur.val ){
-                pre = pre.next;
-            }
-            //insert between pre and pre.next
-            cur.next = pre.next;
-            pre.next = cur;
-            pre = newHead;
-            cur = next;
+        ListNode dummy = new ListNode(0);
+        ListNode after = dummy, front = head, next;
+        while (front != null) {
+            //next pointer is always points to the next of front pointer
+           next = front.next;
+           //insert cur between after and after.next
+           while (after.next != null && after.next.val < front.val) {
+               after = after.next;
+           }
+            after.next = front;
+            front.next = after.next;
+            after = dummy;
+            front = next;
         }
-
-        return newHead.next;
+        return dummy.next;
     }
 
     static public class Test {
@@ -35,8 +30,8 @@ public class InsertionSortList {
         static public void randomTest() {
             ListNode head = new ListNode(1);
             head.next = new ListNode(4);
-            head.next.next = new ListNode(3);
-            head.next.next.next = new ListNode(2);
+            head.next.next = new ListNode(2);
+            head.next.next.next = new ListNode(3);
             ListNode newHead = _solution.insertionSortList(head);
             for (ListNode ptr = newHead; ptr != null; ptr = ptr.next) {
                 System.out.println(ptr.val);
