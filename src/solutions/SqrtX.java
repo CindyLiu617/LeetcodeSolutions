66a024650967d85cace66a7a2ec5280cd1dc696a
CindyLiu617 <yunjia.g@gmail.com>
2017-02-06 17:10:53 +0800

    Solution for Sqrt(x) -- Leetcode Binary Search, Newton‘s Method

--- src/solutions/SqrtX.java
+++ src/solutions/SqrtX.java
@@ -10,26 +10,43 @@ public class SqrtX {
         if(x==0) {
             return 0;
         }
-        int l=1;
-        int r=x/2+1;
-        while(l<=r) {
-            int m = (l+r)/2;
-            if(m<=x/m && x/(m+1)<m+1) {
-                return m;
-            }
-            if(x/m<m) {
-                r = m-1;
+        long left = 0, right = x / 2 + 1;
+        while (left <= right) {
+            long mid = (left + right) / 2, sqrt = mid * mid;
+            if (sqrt == x) {
+                return (int)mid;
+            } else if (sqrt > x) {
+                right = mid - 1;
             } else {
-                l = m+1;
+                left = mid + 1;
             }
         }
-        return 0;
+        return (int)right;
     }
+
+    //牛顿迭代，逼近求方根 因为要求x2 = n的解，令f(x)=x2-n，相当于求解f(x)=0的解，可以求出递推式如下：
+    //xi+1=xi - (xi2 - n) / (2xi) = xi - xi / 2 + n / (2xi) = xi / 2 + n / 2xi =
+    // (xi + n/xi) / 2
+    public int mySqrt2(int x) {
+        if (x < 0) {
+            return -1;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        int rst = 1, pre = 0;
+        while (rst != pre) {
+            pre = rst;
+            rst = (x / rst + rst) / 2;
+        }
+        return rst;
+    }
+
     static public class Test {
         static private SqrtX _solution = new SqrtX();
 
         static public void randomTest() {
-            System.out.println(_solution.mySqrt(3));
+            System.out.println(_solution.mySqrt2(5));
         }
     }
 }
